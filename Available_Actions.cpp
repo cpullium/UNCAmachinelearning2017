e8471e7571143bdfefb6242335487e606f53ea00
Corey Pullium <cpullium@unca.edu>
2016-10-19 14:05:25 -0400

    Update

--- Available_Actions.cpp
+++ Available_Actions.cpp
@@ -1,42 +1,55 @@
 #include "Arduino.h"
-int Valid_Actions[8];
-int Actions_Num;
-int Vaild_Actions_Prime[8];
-int Actions_Num_Prime;
 
-int S1 = 2;//Set initial state (2,2)
-int S2 = 2;
-int S1_Prime;
-int S2_Prime;
+extern int Valid_Actions[8]; 		
+extern int Actions_Num;				//index of the maximum valid action
+extern int Vaild_Actions_Prime[8];
+extern int Actions_Num_Prime;		//index of the maximum valid action in next state
 
-int Available_Actions(State1, State2, Action_Type){
+extern int S1_Prime;         //Temp for possible states
+extern int S2_Prime;        //Temp for possible states
 
+/* Function Description:
+	This function looks at each possible next states and it's action pair. If the state is within
+	the boundry conditions of the state space, it puts it's action pair into a global array.
+	It also updates a global integer that hold the number of actions put into that array.  
+   Written by: 
+	Corey Pullium */
+void Available_Actions(State1, State2, Action_Type){
+	int temp1, temp2;
 	int z[3] = {-1,0,1};
 	int Action_Being_Checked;
-	int Array_Adress;
+
+	temp1 = S1_Prime; //store current value
+	temp2 = S2_Prime; //store current value
 	
-	if (Action_Type == 0) Actions_Num = 0;
-	else Actions_Num_Prime = 0; 
+	if (Action_Type == 0) Actions_Num = 0;      //case of rewriting action vector, reset index/max
+	else Actions_Num_Prime = 0;                //case of rewriting action_prime vector, reset index/max
 
-	for(int i=0; i<3; i++){
-		for (int j=0; j<3; j++){
+	for(int i=0; i<3; i++){				   //step through S1 possibilities 
+		for (int j=0; j<3; j++){		  //step through S2 possibilities
 
-			S1_Prime = S1 + z[i];
-			S2_Prime = S2 + z[j];
+			S1_Prime = State1 + z[i];		//caculate next possible state address for s1
+			S2_Prime = State2 + z[j];		//caculate next possible state address for S2
 
-			if(S1_Prime != S1 && S2_Prime != S2){ //If not staying in the same space
+			if(S1_Prime != State1 && S2_Prime != State2){     //Avoid one case, where both states stay the same
 
-				if(S1_Prime >= 0 && S1_Prime <= 4){ //Within S1 Boundrys
-					if(S2_Prime >= 0 && S2_Prime <= 4){
+				if(S1_Prime >= 0 && S1_Prime <= 4){      //Is it within S1 Boundaries?
+					if(S2_Prime >= 0 && S2_Prime <= 4){ //Is it within S2 Boundaries?
 
-							if (Action_Type == 0) Vaild_Actions[Actions_Num] = a;
-							else Valid_Actions_Prime[Actions_Num_Prime] = a;
-							Array_Adress++
-							
+							if (Action_Type == 0){     					//if looking for actions in current state
+								Vaild_Actions[Actions_Num] = Action_Being_Checked; //put approved action in array 
+								Actions_Num++; 								  //index/max of the array increments 
+							}
+							else { 											   //if looking for actions in surrounding states
+								Valid_Actions_Prime[Actions_Num_Prime] = Action_Being_Checked; //put approved action in array
+								Actions_Num_Prime++; 								      //index/max of the array increments 
+							}
 					}
-				}
+				} 
 				Action_Being_Checked++; //As long as it's not the same space, increment 
-			}	
+			} 
 		}
-	}
-}
+	} 	
+	S1_Prime = temp1; //restore values
+	S2_Prime = temp2; //restore values
+} //end Function
--- Q_update.cpp
+++ Q_update.cpp
@@ -1,5 +1,37 @@
+#include "Arduino.h"
+
+extern float Q[5][5][8];
+extern unsigned int N[5][5][8];
+extern int Valid_Actions[8]; 		
+extern int Actions_Num;				//index of the maximum valid action
+extern int Vaild_Actions_Prime[8];
+extern int Actions_Num_Prime;		//index of the maximum valid action in next state
+extern float alpha; 
+extern int K;
+
+extern int S1_Prime;         //Temp for possible states
+extern int S2_Prime;        //Temp for possible states
+
+
 void Q_Update(){
-  Act_Num = Available_Actions(S1, S2, ACT);
-  for int i
-    Q[S1][S2][a] = (1-alpha)*Q[S1][S2][a] + alpha(Reward() + Qmax + (k/N[............. 
+  int a = 0;
+  int Reward = 0;
+  int beta; 
+  beta = (1-alpha);
+
+  for(int i; i<Actions_Num; i++){
+  	a = Valid_Actions[i];
+
+  	S_Given_A(a);
+  	Available_Actions(S1_Prime,S2_Prime,ACT_PRIME);
+  	for (int j=0; j<Actions_Num_Prime; j++){
+  		S_Given_A(Valid_Actions[j]);
+  		A_Prime = Qmax(S1_Prime,S2_Prime);
+  		qmax = Q[S1_Prime][S2_Prime][A_Prime];
+  	}
+  	Reward = if(a == action_taken) ? R : 0;
+  	oldPerspective = Q[S1][S2][a];
+  	newPerspective = Reward + qmax + K/N[S1_Prime][S2_Prime][A_Prime];
+  	Q[S1][S2][a] = beta*oldPerspective + alpha*newPerspective;
+  }
 }
